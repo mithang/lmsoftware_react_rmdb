eccecbb9ae63ac20accc1276a120321ad5157acd
minh.tranvan <minh.tranvan@hdwebsoft.io>
2022-09-13 22:56:44 +0700

    add test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-it('renders without crashing', ()=>{
-const div = document.createElement('div');
-ReactDOM.render(<App/>,div);
+
+// it('renders without crashing', ()=>{
+// const div = document.createElement('div');
+// ReactDOM.render(<App/>,div);
+// });
+
+describe('App component', ()=>{
+    test('1=1',()=>{
+        expect(1).to.equal(1);
+    });
+
+    test('1#2',()=>{
+        expect(1).to.equal(2);
+    });
+
 });
